d5d3f75c00752cd6198aa08909bbe3f3a6a88639
Gichan Hwang <rlcks1735@gmail.com>
2021-04-16 00:31:16 +0900

    initial commit

---
+++ src/foo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "funcs.h"
+
+void foo(void){
+	printf("Hello, foo world!!!!!\n");
+}
---
+++ src/goo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "funcs.h"
+
+void goo(void){
+	printf("Hello, goo world!!\n");
+}
